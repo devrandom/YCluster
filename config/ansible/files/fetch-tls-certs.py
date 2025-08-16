d27ecaac37bf84291bcfc1d243d971a000bbb929
dev random <devrandom01@proton.me>
2025-08-16 17:36:01 +0200

    spurious update message

--- config/ansible/files/fetch-tls-certs.py
+++ config/ansible/files/fetch-tls-certs.py
@@ -27,11 +27,43 @@ def main():
     key_value, _ = client.get('/cluster/tls/key')
     
     if cert_value and key_value:
-        with open('/etc/nginx/ssl/cert.pem', 'w') as f:
-            f.write(cert_value.decode())
-        with open('/etc/nginx/ssl/key.pem', 'w') as f:
-            f.write(key_value.decode())
-        print('Certificates updated')
+        cert_content = cert_value.decode()
+        key_content = key_value.decode()
+        
+        cert_path = '/etc/nginx/ssl/cert.pem'
+        key_path = '/etc/nginx/ssl/key.pem'
+        
+        # Check if files exist and compare content
+        cert_changed = True
+        key_changed = True
+        
+        try:
+            with open(cert_path, 'r') as f:
+                existing_cert = f.read()
+            cert_changed = existing_cert != cert_content
+        except FileNotFoundError:
+            pass  # File doesn't exist, so it's a change
+        
+        try:
+            with open(key_path, 'r') as f:
+                existing_key = f.read()
+            key_changed = existing_key != key_content
+        except FileNotFoundError:
+            pass  # File doesn't exist, so it's a change
+        
+        # Only write if content has changed
+        if cert_changed:
+            with open(cert_path, 'w') as f:
+                f.write(cert_content)
+        
+        if key_changed:
+            with open(key_path, 'w') as f:
+                f.write(key_content)
+        
+        if cert_changed or key_changed:
+            print('Certificates updated')
+        else:
+            print('Certificates unchanged')
     else:
         print('No certificates found in etcd')
         sys.exit(1)
